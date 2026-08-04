b511cf48ba3cc7087c978f848b1fbee04336cf30
deepikakethavath8688 <deepikakethavath2@gmail.com>
2026-08-04 16:39:50 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,11 @@
+public class Pattern1 {
+    public static void main (String[] ages) {
+        int  n  = 5;
+
+        for ( int i = 1;i <=n;i++) {
+            for( int j = 1; j   <=n-i+1; j++){
+                System.out.print("*");
+            }  System.out.println();
+        }
+     }
+}
---
+++ SumDigit.java
@@ -0,0 +1,16 @@
+public class SumDigit{
+    public static void main(String[] args){
+        int sum = 0;
+        int a = 100;
+        int b = 20;
+        sum = addNumbers( a , b );
+        System.out.println("a value is :" + a);
+        System.out.println( "sum of number =" + sum);
+    }
+     static int addNumbers( int x , int y) {
+        // a = 5;
+        return x + y;
+    }
+}
+
+
